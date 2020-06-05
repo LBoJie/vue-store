6ae0aedde41ee4593299fa3d3b240137b3b0b54a
LBoJie <s19980303@icloud.com>
2020-06-05 16:31:43 +0800

    clear

--- src/components/frontend/Footer.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="bg-dark p-3 text-center">
-    <img src="https://img.icons8.com/dusk/64/000000/wood.png" />
-    <p class="pt-2 text-white">本網頁所載的所有資料、商標、標誌、圖像、僅供個人學習之用，非商業用途。</p>
-  </div>
-</template>
-<script>
-export default {};
-</script>
--- src/filters/timeFormat.js
+++
@@ -1,6 +0,0 @@
-export default function (dateTime) {
-  const date = new Date(dateTime * 1000);
-  let dataValues2 = [date.getFullYear(), date.getMonth() + 1, date.getDate()];
-  dataValues2 = dataValues2.toString();
-  return dataValues2;
-}
